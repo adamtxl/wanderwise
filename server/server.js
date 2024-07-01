c33c9b48015a8509f0613b36f37234e8bc6f8c97
Adam Troxell <adamtxl@hotmail.com>
2024-07-01 14:01:05 -0500

    Testing Deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 
 const cors = require('cors')
