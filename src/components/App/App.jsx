6a908c39dca1fb9ecd8b65e4754a92eb90cd0738
Adam Troxell <adamtxl@hotmail.com>
2024-09-29 11:37:16 -0500

    All functionality restored

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -30,64 +30,61 @@ import PastTrips from '../Trips/PastTrips';
 import AdminPage from '../AdminPage/AdminPage';
 import NonAdminLanding from '../AdminPage/NonAdminLanding';
 
-
-
 function App() {
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch({ type: 'FETCH_USER' });  // Fetch the user's session when the app loads
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch({ type: 'FETCH_USER' }); // Fetch the user's session when the app loads
+	}, [dispatch]);
 
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/user" element={<ProtectedRoute />}>
-            <Route path="/user" element={<UserPage />} />
-          </Route>
-          <Route path="/info" element={<ProtectedRoute />}>
-            <Route path="/info" element={<InfoPage />} />
-          </Route>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/trips" element={<ProtectedRoute />}>
-            <Route path="/trips" element={<Trips />} />
-          </Route>
-          <Route path="/edit-create-trips" element={<ProtectedRoute />}>
-            <Route path="/edit-create-trips" element={<EditCreateTrips />} />
-          </Route>
-          <Route path="/create-daily-itinerary" element={<ProtectedRoute />}>
-            <Route path="/create-daily-itinerary" element={<DailyItinerary />} />
-          </Route>
-          <Route path="/packing-list" element={<ProtectedRoute />}>
-            <Route path="/packing-list" element={<PackingList />} />
-          </Route>
-          <Route path="/map" element={<ProtectedRoute />}>
-            <Route path="/map" element={<Map />} />
-          </Route>
-          <Route path="/map-page" element={<ProtectedRoute />}>
-            <Route path="/map-page" element={<MapPage />} />
-          </Route>
-          <Route path="/past-trips" element={<ProtectedRoute />}>
-            <Route path="/past-trips" element={<PastTrips />} />
-          </Route>
-          <Route path="/admin" element={<AdminRoute />}>
-            <Route path="/admin" element={<AdminPage />} />
-          </Route>
-          <Route path="/non-admin-landing" element={<NonAdminLanding />} />
-          <Route path="/trip-details/:tripId" element={<ProtectedRoute />}>
-            <Route path="/trip-details/:tripId" element={<TripDetails />} />
-          </Route>
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Nav />
+				<Routes>
+					<Route path='/' element={<LandingPage />} />
+					<Route path='/about' element={<AboutPage />} />
+					<Route path='/user' element={<ProtectedRoute />}>
+						<Route path='/user' element={<UserPage />} />
+					</Route>
+					<Route path='/info' element={<ProtectedRoute />}>
+						<Route path='/info' element={<InfoPage />} />
+					</Route>
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+					<Route path='/trips' element={<ProtectedRoute />}>
+						<Route path='/trips' element={<Trips />} />
+					</Route>
+					<Route path='/edit-create-trips' element={<ProtectedRoute />}>
+						<Route path='/edit-create-trips' element={<EditCreateTrips />} />
+					</Route>
+					<Route path='/create-daily-itinerary' element={<ProtectedRoute />}>
+						<Route path='/create-daily-itinerary' element={<DailyItinerary />} />
+					</Route>
+					<Route path='/packing-list/:tripId' element={<ProtectedRoute />}>
+						<Route path='/packing-list/:tripId' element={<PackingList />} />
+					</Route>
+					<Route path='/map' element={<ProtectedRoute />}>
+						<Route path='/map' element={<Map />} />
+					</Route>
+					<Route path='/map-page' element={<ProtectedRoute />}>
+						<Route path='/map-page' element={<MapPage />} />
+					</Route>
+					<Route path='/past-trips' element={<ProtectedRoute />}>
+						<Route path='/past-trips' element={<PastTrips />} />
+					</Route>
+					<Route path='/admin' element={<AdminRoute />}>
+						<Route path='/admin' element={<AdminPage />} />
+					</Route>
+					<Route path='/non-admin-landing' element={<NonAdminLanding />} />
+					<Route path='/trip-details/:tripId' element={<ProtectedRoute />}>
+						<Route path='/trip-details/:tripId' element={<TripDetails />} />
+					</Route>
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
